ad2902a77b48a1cab174b24810b7db28bf52709e
Sevo98 <sevo98.sk@gmail.com>
2022-12-04 15:15:00 +0500

    Отрефакторил начальный код. Перенес подключение к Gmail в GmailConnector.java, методы запроса почты в GmailApiController.java. Написал метод отображения всех писем из входящих. Написал проверку на пустой почтовый ящик.

---
+++ src/main/java/gmail/GmailApiController.java
@@ -0,0 +1,94 @@
+package gmail;
+
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.StringUtils;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GmailApiController {
+    private static final String user = "me";
+
+    public static void printMessageTo() throws IOException, GeneralSecurityException {
+        try {
+            Gmail service = GmailConnector.auth();
+
+            //Access Gmail
+            Gmail.Users.Messages.List request = service.users().messages().list(user).setQ("to: " + "jenleroy8@gmail.com");
+
+            ListMessagesResponse messagesResponse = request.execute();
+            request.setPageToken(messagesResponse.getNextPageToken());
+
+            //Get ID of the email you are looking for
+            String messageId = messagesResponse.getMessages().get(0).getId();
+            Message message = service.users().messages().get(user, messageId).execute();
+            //Print email
+            String emailBody = StringUtils.newStringUtf8(Base64.decodeBase64(message.getPayload().getParts().get(0).getBody().getData()));
+            System.out.println("EMail: " + emailBody);
+        } catch (NullPointerException e) {
+            System.out.println("Letters not found");
+            ;
+        }
+    }
+
+    public static void printMessageFrom() throws IOException, GeneralSecurityException {
+        try {
+            Gmail service = GmailConnector.auth();
+
+            //Access Gmail
+            Gmail.Users.Messages.List request = service.users().messages().list(user).setQ("from: " + "jenleroy8@gmail.com");
+
+            ListMessagesResponse messagesResponse = request.execute();
+            request.setPageToken(messagesResponse.getNextPageToken());
+
+            //Get ID of the email you are looking for
+            String messageId = messagesResponse.getMessages().get(0).getId();
+            Message message = service.users().messages().get(user, messageId).execute();
+            //Print email
+            String emailBody = StringUtils.newStringUtf8(Base64.decodeBase64(message.getPayload().getParts().get(0).getBody().getData()));
+            System.out.println("EMail: " + emailBody);
+        } catch (NullPointerException e) {
+            System.out.println("Letters not found");
+        }
+
+    }
+
+    public static void listMessages() throws IOException, GeneralSecurityException {
+        try {
+            Gmail service = GmailConnector.auth();
+
+            //Access Gmail
+            Gmail.Users.Messages.List request = service.users().messages().list(user).setQ("from: " + "jenleroy8@gmail.com");
+
+            ListMessagesResponse messagesResponse = request.execute();
+            request.setPageToken(messagesResponse.getNextPageToken());
+
+
+            //Get ID of the email you are looking for
+            String messageId = messagesResponse.getMessages().get(0).getId();
+            ListMessagesResponse response = service.users().messages().list("me").execute();
+
+            List<Message> messages = new ArrayList<Message>();
+            messages.addAll(response.getMessages());
+
+            String pageToken = response.getNextPageToken();
+            response = service.users().messages().list("me").setPageToken(pageToken).execute();
+
+            //Print email
+            for (Message message : messages) {
+                System.out.println(message.getId());
+                Message test = service.users().messages().get("me", message.getId()).execute();
+                System.out.println(StringUtils.newStringUtf8(Base64.decodeBase64(test.getPayload().getParts().get(0).getBody().getData())));
+            }
+
+        } catch (NullPointerException e) {
+            System.out.println("Letters not found");
+        }
+
+    }
+}
---
+++ src/main/java/gmail/GmailConnector.java
@@ -0,0 +1,94 @@
+package gmail;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.gmail.Gmail;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.GeneralSecurityException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class GmailConnector {
+    private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    private static final String user = "me";
+    static Gmail service = null;
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    public static Gmail auth() throws IOException, GeneralSecurityException {
+        InputStream in = GmailConnector.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        //Credential builder
+        Credential authorize = new GoogleCredential.Builder().setTransport(GoogleNetHttpTransport.newTrustedTransport())
+                .setJsonFactory(JSON_FACTORY)
+                .setClientSecrets(clientSecrets.getDetails().getClientId().toString(), clientSecrets.getDetails().getClientSecret().toString())
+                .build()
+                .setAccessToken(getAccessToken())
+                .setRefreshToken("1//0cn2Ktpr57KPdCgYIARAAGAwSNwF-L9IrHUfLzWZsrwl1MH_6Camlgnl6LpEG1pxmfTL-lbjvm92TwaU_ywJo6tFb2jBPyUP5JR0");
+
+        //Gmail Service
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        return service;
+    }
+
+    public static String getAccessToken() {
+        try {
+            Map<String, Object> params = new LinkedHashMap<>();
+            params.put("grant_type", "refresh_token");
+            params.put("client_id", "18306162202-qpvhpauraicd80efp009u52cfrsod9k4.apps.googleusercontent.com");
+            params.put("client_secret", "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");
+            params.put("refresh_token",
+                    "1//0cn2Ktpr57KPdCgYIARAAGAwSNwF-L9IrHUfLzWZsrwl1MH_6Camlgnl6LpEG1pxmfTL-lbjvm92TwaU_ywJo6tFb2jBPyUP5JR0");
+
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                if (postData.length() != 0) {
+                    postData.append('&');
+                }
+                postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                postData.append('=');
+                postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+            }
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+            URL url = new URL("https://accounts.google.com/o/oauth2/token");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+            con.setRequestMethod("POST");
+            con.getOutputStream().write(postDataBytes);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuffer buffer = new StringBuffer();
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                buffer.append(line);
+            }
+
+            JSONObject json = new JSONObject(buffer.toString());
+            String accessToken = json.getString("access_token");
+            return accessToken;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
